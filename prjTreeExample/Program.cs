b4433af38dd4e68ddc65dcbb062d85dfaf5ae1e3
garethlelie <st10076171@vcconnect.edu.za>
2025-09-01 18:02:37 +0200

    Add project files.

---
+++ prjTreeExample/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace prjTreeExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>();
+
+            tree.Root = new TreeNode<int>()
+            {
+                Data = 100
+            };
+
+            tree.Root.Children = new List<TreeNode<int>>
+            {
+                new TreeNode<int> { Data = 100, Parent = tree.Root },
+                new TreeNode<int> { Data = 1, Parent = tree.Root },
+                new TreeNode<int> { Data = 150, Parent = tree.Root },
+            };
+
+            tree.Root.Children[2].Children = new List<TreeNode<int>>
+            {
+                new TreeNode<int> { Data = 30, Parent = tree.Root.Children[2] },
+            };
+
+            tree.Root.Children[0].Children = new List<TreeNode<int>>
+            {
+                new TreeNode<int> { Data = 8, Parent = tree.Root.Children[0] },
+                new TreeNode<int> { Data = 11, Parent = tree.Root.Children[0] },
+            };
+
+            tree.Root.Children.Add(new TreeNode<int>()
+            {
+                Data = 70, Parent = tree.Root
+            });
+
+            tree.PrintTree(tree.Root);
+
+            Console.WriteLine("Data of Child: " + tree.Root.Children[0].Children[1].Data.ToString());
+            TreeNode<int> Node = tree.FindNode(tree.Root, 1);
+            tree.PrintTree(tree.Root, Node);
+        }
+    }
+}
---
+++ prjTreeExample/Tree.cs
@@ -0,0 +1,51 @@
+﻿namespace prjTreeExample
+{
+    class Tree<T>
+    {
+        public TreeNode<T> Root { get; set; }
+
+        public void PrintTree(TreeNode<T> node, string indent = "", bool last = true)
+        {
+            if  (node == null) return;
+
+            Console.WriteLine($"{indent} +- {node.Data}");
+            indent += last ? " " : "| ";
+
+            for (int i = 0; i < node.Children.Count; i++)
+            {
+                PrintTree(node.Children[i], indent, i == node.Children.Count - 1);
+            }
+        }
+
+        public TreeNode<T> FindNode(TreeNode<T> node, T value)
+        {
+            if (node == null) return null;
+
+            if (node.Data.Equals(value)) return node;
+
+            foreach (var child in node.Children)
+            {
+                var result = FindNode(child, value);
+                if (result != null) return result;
+            }
+            return null;
+        }
+
+        public void PrintTree(TreeNode<T> node, TreeNode<T> highlightNode, string indent = "", bool last = true)
+        {
+            if  (node == null) return;
+
+            if (node.Equals(highlightNode))
+                Console.WriteLine($"{indent} +- [{node.Data}");
+            else
+                Console.WriteLine($"{indent} +- {node.Data}");
+
+            indent += last ? " " : "| ";
+
+            for (int i = 0; i < node.Children.Count; i++)
+            {
+                PrintTree(node.Children[i], highlightNode, indent, i == node.Children.Count - 1);
+            }
+        }
+    }
+}
